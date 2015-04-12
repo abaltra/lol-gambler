727018e9f9a6f04f3a4825f1947b40db4f5275e2
Fabian Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2015-04-12 14:43:56 -0300

    Login fixed, and parameters names weren't in the template, also fixed!

--- passport/login.js
+++ passport/login.js
@@ -3,18 +3,18 @@ var User = require('../models/user');
 var bCrypt = require('bcrypt-nodejs');
 
 module.exports = function(passport){
-
 	passport.use('login', new LocalStrategy({
             passReqToCallback : true
         },
         function(req, username, password, done) { 
             // check in mongo if a user with username exists or not
-            console.log("Username : "+username);
             User.findOne({ 'username' :  username }, 
                 function(err, user) {
                     // In case of any error, return using the done method
-                    if (err)
+                    if (err){
+                        console.log("Error! "+err);
                         return done(err);
+                    }
                     // Username does not exist, log the error and redirect back
                     if (!user){
                         console.log('User Not Found with username '+username);
