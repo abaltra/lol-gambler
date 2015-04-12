63ec54c7b319c64887a977250f4368ff7fb043b2
Fabian Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2015-04-12 20:28:13 -0300

    Champion selection in bets

---
+++ public/js/helpers.js
@@ -0,0 +1,5 @@
+function selectChampion(imgUrl, name, subtitle) {
+    document.getElementById("championImg").src		=imgUrl;
+    document.getElementById("championName").text	=name;
+    document.getElementById("championSubtitle").text=subtitle;
+}
